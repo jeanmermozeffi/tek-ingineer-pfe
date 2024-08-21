89f2f7a3c2281c53a5ace0441a8f36024c3e71c1
ddma <datasci@adisa.digital>
2024-08-21 11:54:32 +0000

    Etape netoyyage de donne

---
+++ data_test.py
@@ -0,0 +1,14 @@
+data_test = {
+    'Marque': ['Ineos'],
+    'Modele': ['Grenadier'],
+    'Annee': [2024],
+    'Vehicule': ['Ineos Grenadier 3.0 T 286ch Fieldmaster Edition'],
+    'Prix': [82490.0],
+    'Date Publication': ['2022-04-01'],
+    'Resumer': [
+        '{"energie": "Essence", "puissance_commerciale": 286, "puissance_fiscale": 0, "consommation_mixte": 14.4, '
+        '"emission_de_co2": 325, "boite_de_vitesses": "Automatique", "carrosserie": "4*4/SUV/Crossovers", '
+        '"date_de_fin_de_commercialisation": "-", "Immatriculation": "7e546927-d3e3-477f-8971-b0cd70187264", '
+        '"Object_Folder_Resumer": "Vehiculs/Models/INEOS/Resumer"}'
+    ]
+}
